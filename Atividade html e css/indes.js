e67b8f1d0434d683ccad6093e4feb06ab2b24c3c
Paulo Oliveira <paulotfo@gmail.com>
2023-04-25 21:35:08 +0100

    primeiro commit

---
+++ Atividade html e css/indes.js
@@ -0,0 +1,8 @@
+console.log("Olá Mundo!");
+let nome = "Paulo";
+console.error(nome);
+console.warn(nome);
+nome = window.prompt("insira o seu nome:");
+
+console.log(nome);
+let confirmação = window.confirm("Confirma essa ação?");
